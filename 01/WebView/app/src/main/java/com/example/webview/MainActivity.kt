bdd58508e9d7e561d7ba5130eb159b212d9e09bb
LAGABI44 <kaslily25@gmail.com>
2022-07-03 15:49:32 +0900

    update

---
+++ 01/WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.main_webview)
+        myWebView.loadUrl("https://www.google.com")
+    }
+}
